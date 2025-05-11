49ad136bdb813aa549ae3439f81454756c99abac
wich011 <tatotello210@gmail.com>
2025-05-11 15:28:29 -0600

    Delet: carpeta

--- moodify-fe/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { Dashboard } from './Dashboard'
-export { NavigationTabs } from './NavigationTabs'
-export { Analyze } from './Analyze'
-export { Home } from './home'
-export { History } from './History'
-export { Login } from './Login'
-export {CreateAccount} from './CreateAccount'
-export { EmotionModal } from './EmotionModal'
-
--- moodify-fe/src/views/index.js
+++
@@ -1,4 +0,0 @@
-export { LandingPage } from './LandingPage'
-export { DashboardPage } from './DashboardPage'
-export { LoginPage } from './LoginPage'
-export { CreateAccountPage } from './CreateAccountPage'
